9f6baba323ea958ecdee98a7d4ff1ae91da8fcca
aghanr <aghanrr@gmail.com>
2022-11-28 17:00:41 +0700

    commit ketujuh

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,10 +6,10 @@ import "./About.css";
 export default function About(){
     return <Layout>
          <div className="contentAbout">
-                <img src="/images/agha.png" className="contentAboutImg" alt="rocket"></img>
+                <img src="/images/agha 400x400.png" className="contentAboutImg" alt="rocket"></img>
                 <Gap height={20}/>
                 <h2 className="contentAboutTitle">Tentang AIRBNBGHA</h2>
-                <p className="contentAboutBody">Aplikasi ini dikembangkan oleh Anugrah Nur Rachmat, NIM 21120120130126, dengan tujuan memenuhi tugas akhir Praktikum Pemrograman Perangkat Bergerak</p>
+                <p className="contentAboutBody">Aplikasi ini dikembangkan oleh Anugerah Nur Rachmat, NIM 21120120130126, dengan tujuan memenuhi tugas akhir Praktikum Pemrograman Perangkat Bergerak</p>
             </div>
     </Layout>
 }
